b676ee69b3362d5868304cdddcee5a62087f29a2
rahulsinghpharthyal <rahulpharthyal04@gmail.com>
2025-03-25 16:17:46 +0530

    set up controller and route

--- src/app.js
+++ src/app.js
@@ -10,9 +10,20 @@ app.use(cors({
 
 app.use(express.json()); // we use limit also limi:16kb
 
-//for
+//for encode the url 
 app.use(express.urlencoded({extended: true})); // we use limit also limi:16kb
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+// routes import 
+
+import userRoute from './routes/user.route.js';
+
+
+// routes declaration
+app.use('/api/v1/users', userRoute);
+
+
 
 export default app;
